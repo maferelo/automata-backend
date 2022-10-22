995a136594336fd20aa97e86cfdc7fe781210173
maferelo <maferelo13@gmail.com>
2022-10-22 17:40:20 +0000

    nox tests on pre-commit

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -6,9 +6,9 @@ git init .
 
 poetry run pre-commit install --install-hooks
 
-echo "source $(poetry env info --path)/bin/activate" >> ~/.bashrc
+poetry run pre-commit run -a
 
-poetry run nox -r
+echo "source $(poetry env info --path)/bin/activate" >> ~/.bashrc
 
 # Apply migrations
 #alembic upgrade head
