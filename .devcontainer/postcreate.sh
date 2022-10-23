7b670554524ef5ef6e60cbdb43d5360d1f654542
maferelo <maferelo13@gmail.com>
2022-10-23 00:03:48 +0000

    reorder postcreate

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -2,13 +2,12 @@
 
 poetry install
 
+echo "source $(poetry env info --path)/bin/activate" >> ~/.bashrc
+
+#poetry run alembic upgrade head
+
 git init .
 
 poetry run pre-commit install --install-hooks
 
 poetry run pre-commit run -a
-
-echo "source $(poetry env info --path)/bin/activate" >> ~/.bashrc
-
-# Apply migrations
-#alembic upgrade head
