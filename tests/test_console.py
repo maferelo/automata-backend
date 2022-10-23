1ecb5e7c7de646246b8dcb8848636ec48703f401
maferelo <maferelo13@gmail.com>
2022-10-23 02:03:33 +0000

    wikipedia language

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,17 +1,13 @@
 import click.testing
 import pytest
+import requests
 
 from app import console
 
 
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("app.wikipedia.random_page")
 
 
 @pytest.fixture
@@ -30,6 +26,12 @@ def test_main_invokes_requests_get(runner, mock_requests_get):
     assert mock_requests_get.called
 
 
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
+
+
 def test_main_prints_title(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert "Lorem Ipsum" in result.output
@@ -44,3 +46,8 @@ def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
