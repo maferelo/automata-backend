e04189f95f2e74aef29ee9d3b017aee9e8e9a333
maferelo <maferelo13@gmail.com>
2022-12-04 22:55:25 +0000

    Basic typing

--- src/app/console.py
+++ src/app/console.py
@@ -16,7 +16,7 @@ from . import wikipedia
     show_default=True,
 )
 @click.version_option(version=__version__)
-def main(language):
+def main(language: str) -> None:
     """The hypermodern Python project."""
     data = wikipedia.random_page(language=language)
 
