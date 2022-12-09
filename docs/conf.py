b763a4a4a575d3101cf8fcb6ff165da54fe678cd
maferelo <maferelo13@gmail.com>
2022-12-09 20:55:25 +0000

    pylint ignore directory
    ignore with vscode config, no need on pyproject.toml due to pre-commit

--- docs/conf.py
+++ docs/conf.py
@@ -4,5 +4,10 @@ from datetime import datetime
 project = "Hypermodern Python"
 author = "Claudio Jolowicz"
 copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
+extensions = [
+    "sphinx.ext.toctree",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
 html_static_path = ["_static"]
