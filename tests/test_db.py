401d6338cba0eecb1ada7d54a401804232fd4402
Felipe Restrepo <maferelo13@gmail.com>
2022-12-27 05:22:38 +0000

    mock everything as tests run parallel
    moc basesettings and databases module

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,8 +2,11 @@
 from unittest import mock
 from unittest.mock import Mock
 
-with mock.patch("databases.Database") as mock_method:
-    from src.app import db
+with mock.patch("databases.Database") as _mock_db:
+    with mock.patch("pydantic.BaseSettings") as mock_settings:
+        p = mock.PropertyMock(return_value="sqlite://")
+        type(mock_settings).db_url = p
+        from src.app import db
 
 
 def test_get_db(mock_db: Mock) -> None:
