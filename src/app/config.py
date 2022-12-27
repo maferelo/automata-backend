340fb8986d714ae8bf656334faa861d4dbba4b4b
Felipe Restrepo <maferelo13@gmail.com>
2022-12-27 19:08:18 +0000

    config typing
    fi x wrong return type

--- src/app/config.py
+++ src/app/config.py
@@ -3,18 +3,18 @@ from pydantic import BaseSettings
 from pydantic import Field
 
 
-def get_settings() -> BaseSettings:
+class Settings(BaseSettings):  # pylint: disable=too-few-public-methods
+    """Application settings."""
+
+    db_url: str = Field(..., env="DATABASE_URL")
+    db_engine_pool_size: int = Field(19, env="DATABASE_ENGINE_POOL_SIZE")
+    db_engine_max_overflow: int = Field(0, env="DATABASE_ENGINE_MAX_OVERFLOW")
+    telegram_chat_id: str = Field(..., env="TELEGRAM_CHAT_ID")
+    telegram_token: str = Field(..., env="TELEGRAM_TOKEN")
+
+
+def get_settings() -> Settings:
     """Get application settings."""
-
-    class Settings(BaseSettings):  # pylint: disable=too-few-public-methods
-        """Application settings."""
-
-        db_url: str = Field(..., env="DATABASE_URL")
-        db_engine_pool_size: int = Field(19, env="DATABASE_ENGINE_POOL_SIZE")
-        db_engine_max_overflow: int = Field(0, env="DATABASE_ENGINE_MAX_OVERFLOW")
-        telegram_chat_id: str = Field(..., env="TELEGRAM_CHAT_ID")
-        telegram_token: str = Field(..., env="TELEGRAM_TOKEN")
-
     application_settings = Settings()
 
     # Required for postgres Heroku deprecated postgres naming
