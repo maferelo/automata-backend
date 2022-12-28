aaa3899e7bcd31367f4c6569193df53119fbbe12
Felipe Restrepo <maferelo13@gmail.com>
2022-12-28 20:16:34 +0000

    fastapi jinja templates and static files
    home page and page are responded with htmlresponse

--- website/main.py
+++ website/main.py
@@ -3,19 +3,33 @@
 https://levelup.gitconnected.com/building-a-website-starter-with-fastapi-92d077092864
 """
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def home():
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
     """Home page."""
-    data = {"text": "hi"}
+    data = {"page": "Home page"}
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/healthcheck")
+async def healthcheck():
+    """Healthcheck."""
+    data = {"status": "ok"}
     return {"data": data}
 
 
-@app.get("/page/{page_name}")
-async def page(page_name: str):
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def page(request: Request, page_name: str):
     """Page."""
     data = {"page": page_name}
-    return {"data": data}
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
