d96c1a328ffa6cb480fc27a8883ae3081610a105
Felipe Restrepo <maferelo13@gmail.com>
2023-01-22 03:20:31 +0000

    lock deps && Docker cache && nox simplified

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-poetry install
-
 echo "source $(poetry env info --path)/bin/activate" >> ~/.bashrc
 
+# Fix git dubious ownership error
 git config --global --add safe.directory /app
-
-poetry run pre-commit install --install-hooks
+# Fix git unable to undo changes
+git config core.filemode false
 
 alias run="gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --access-logfile - --reload"
-
-#poetry run alembic upgrade head
-
-#poetry run pre-commit run -a
